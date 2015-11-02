105634cb587534b0032fa5eb6930279057e34869
liu946 <liuyang570926881@gmail.com>
2015-11-03 00:17:34 +0800

    mysql-config

--- booksys/settings.py
+++ booksys/settings.py
@@ -94,8 +94,12 @@ else:
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': MYSQL_DB,
+        'USER': MYSQL_USER,
+        'PASSWORD': MYSQL_PASS,
+        'HOST': MYSQL_HOST,
+        'PORT': MYSQL_PORT,
     }
 }
 
