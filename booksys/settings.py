e95efda2461a83f20ebb54a565d602421becce86
liu946 <liuyang570926881@gmail.com>
2015-11-03 13:06:34 +0800

    static_file_config

--- booksys/settings.py
+++ booksys/settings.py
@@ -131,7 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static').replace('\\', '/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'bookmanagement/static').replace('\\', '/')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "staticorg"),
+    os.path.join(BASE_DIR, "bookmanagement/static"),
 )
